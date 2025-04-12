f27bcca3305f32452eb2892bb30ea13b5a88ebf6
Eric Monterozo <ericmonterozo@Erics-MacBook-Pro.local>
2025-04-12 20:17:11 +0800

    feat: test ids

--- src/components/FacebookMessage/FacebookMessage.tsx
+++ src/components/FacebookMessage/FacebookMessage.tsx
@@ -2,11 +2,8 @@ import { FacebookProvider, CustomChat } from "react-facebook";
 
 const FacebookMessage = () => {
   return (
-    <FacebookProvider appId={process.env.FACEBOOK_APP_ID ?? ""} chatSupport>
-      <CustomChat
-        pageId={process.env.FACEBOOK_PAGE_ID ?? ""}
-        minimized={false}
-      />
+    <FacebookProvider appId={"9596771340430426"} chatSupport>
+      <CustomChat pageId={"587185991150484"} minimized={true} />
     </FacebookProvider>
   );
 };
